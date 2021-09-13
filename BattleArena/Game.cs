b1c1edc06380672b80ba4c13f9a58ddf459851c3
Terrick Thibodeaux <terrickthibodeaux@gmail.com>
2021-09-13 12:48:59 -0500

    Added Charters Created Functions

--- BattleArena/Game.cs
+++ BattleArena/Game.cs
@@ -23,15 +23,35 @@ namespace BattleArena
         Character[] enemies;
         private int currentEnemyIndex = 0;
         private Character currentEnemy;
+        string name = "";
+
+        //Monsters
+        Character goblin;
+        Character muscleman;
+        Character toad;
+
+        //Player choice
+        Character tank;
+        Character hunter;
 
         
-
+        
+               
 
         /// <summary>
         /// Function that starts the main game loop
         /// </summary>
         public void Run()
-        {
+        {                        
+          Start();
+            while (!gameOver)
+            {
+                DisplayMainMenu();
+                GetPlayerName();
+                CharacterSelection();
+                Battle(ref player, ref goblin);
+                End();
+            } 
         }
 
         /// <summary>
@@ -39,14 +59,45 @@ namespace BattleArena
         /// </summary>
         public void Start()
         {
+            //Character Choice 1
+            tank.name = "Tank" + (name);
+            tank.health = 50.0f;
+            tank.attackPower = 15;
+            tank.defensePower = 30;
+
+            //Character Choice 2
+            hunter.name = "Hunter" + (name);
+            hunter.health = 30.0f;
+            hunter.attackPower = 25.0f;
+            hunter.defensePower = 20.0f;
+
+            //Enemie1
+            goblin.name = "Goblin";
+            goblin.health = 10.0f;
+            goblin.attackPower = 2.0f;
+            goblin.defensePower = 5.0f;
+
+            //Enemie2
+            muscleman.name = "Muscleman";
+            muscleman.health = 30.0f;
+            muscleman.attackPower = 15.0f;
+            muscleman.defensePower = 10.0f;
+
+            //Enemie3
+            toad.name = "Toad";
+            toad.health = 15.0f;
+            toad.attackPower = 8.0f;
+            toad.defensePower = 5.0f;
+
             
         }
-
+        
         /// <summary>
         /// This function is called every time the game loops.
         /// </summary>
         public void Update()
         {
+            
         }
 
         /// <summary>
@@ -54,6 +105,16 @@ namespace BattleArena
         /// </summary>
         public void End()
         {
+            //Ask if you want to play again or end game.
+            int Input = GetInput("Would you like to play again? ", "Yes", "No");
+            if (Input ==1)
+            {
+                gameOver = false;
+            }
+            else if (Input ==2)
+            {
+                gameOver = true;
+            }
         }
 
         /// <summary>
@@ -108,6 +169,7 @@ namespace BattleArena
         /// </summary>
         void DisplayCurrentScene()
         {
+            currentScene = 0;
         }
 
         /// <summary>
@@ -115,6 +177,16 @@ namespace BattleArena
         /// </summary>
         void DisplayMainMenu()
         {
+            //Gets players Choice
+            int Input = GetInput("Welcome to fight club ", "Start Game ", "Quit Game");
+            if (Input == 1)
+            {
+                currentScene = 1;
+            }
+            else if (Input == 2)
+            {
+                gameOver = true;
+            }
         }
 
         /// <summary>
@@ -123,8 +195,20 @@ namespace BattleArena
         /// </summary>
         void GetPlayerName()
         {
-            Console.WriteLine("Welcome To FightCLub!!" "First Rule of fight club dont talk about fight club");
-            Console.WriteLine("")
+            currentScene = 1;
+            Console.WriteLine("Welcome To FightCLub!! First Rule of fight club dont talk about fight club");
+            Console.WriteLine("So whats your name? ");
+            name = Console.ReadLine();
+            Console.WriteLine(name);
+            int Input = GetInput("Are You sure? ", "Yes ", "No");
+            if (Input ==1)
+            {
+                currentScene = 2;
+            }
+            else if (Input ==2)
+            {
+                GetPlayerName();
+            }
         }
 
         /// <summary>
@@ -133,6 +217,21 @@ namespace BattleArena
         /// </summary>
         public void CharacterSelection()
         {
+            currentScene = 2;
+            int Input = GetInput("Pick a Character. ", "Tank ", "Hunter");
+            if(Input == 1)
+            {
+                player = tank;
+                DisplayStats(tank);
+                currentScene = 3;
+            }
+            else if (Input == 2)
+            {
+                player = hunter;
+                DisplayStats(hunter);
+                currentScene = 3;
+            }
+            Console.ReadKey();
         }
 
         /// <summary>
@@ -141,6 +240,10 @@ namespace BattleArena
         /// <param name="character">The character that will have its stats shown</param>
         void DisplayStats(Character character)
         {
+            Console.WriteLine("Name: " + character.name);
+            Console.WriteLine("Health: " + character.health);
+            Console.WriteLine("AttackPower: " + character.attackPower);
+            Console.WriteLine("DefensePower: " + character.defensePower);
         }
 
         /// <summary>
@@ -151,6 +254,17 @@ namespace BattleArena
         /// <returns>The amount of damage done to the defender</returns>
         float CalculateDamage(float attackPower, float defensePower)
         {
+            float damage = attackPower - defensePower;
+
+            if (damage <= 0)
+            {
+                damage = 0;
+            }
+            return damage;
+        }
+        float CalculateDamage(Character attacker, Character defender)
+        {
+            return attacker.attackPower - defender.defensePower;
         }
 
         /// <summary>
@@ -161,13 +275,29 @@ namespace BattleArena
         /// <returns>The amount of damage done to the defender</returns>
         public float Attack(ref Character attacker, ref Character defender)
         {
+            float damageTaken = CalculateDamage(attacker, defender);
+            defender.health -= damageTaken;
+            return damageTaken;
         }
 
         /// <summary>
         /// Simulates one turn in the current monster fight
         /// </summary>
-        public void Battle()
+        public void Battle(ref Character player, ref Character goblin)
         {
+            //Shows Players stats
+            DisplayStats(player);
+            //Shows enemys stats
+            DisplayStats(goblin);
+
+            //Character Attacks enemy
+            float damageTaken = Attack(ref player, ref goblin);
+            Console.WriteLine(goblin + " has taken " + damageTaken);
+
+            //Enemy Attacks Player
+            damageTaken = Attack(ref goblin, ref player);
+            Console.WriteLine(player + " has taken " + damageTaken);
+            
         }
 
         /// <summary>
